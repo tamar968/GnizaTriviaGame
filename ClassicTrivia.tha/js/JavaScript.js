597f50750556e3d6fc5c348bd3c1645e64a7d6bd
tamar968 <52313308+tamar968@users.noreply.github.com>
2020-07-13 01:00:20 +0300

    Add files via upload
    upload project

---
+++ ClassicTrivia.tha/js/JavaScript.js
@@ -0,0 +1,129 @@
+﻿
+//my code
+var questions = userQuestions;
+var questionIndex = 0;
+var question = document.getElementById("question");
+var answer1 = document.getElementById("ans1Content");
+var answer2 = document.getElementById("ans2Content");
+var answer3 = document.getElementById("ans3Content");
+var pointsGroup1 = 0;
+var pointsGroup2 = 0;
+var userTimeUntilMusicStop = timeUntilMusicStop;
+var userTimeUntilViewAnswer = timeUntilViewAnswer;
+var mus = document.getElementById("music");
+var dinor = document.getElementById("dinor");
+
+function nextQustion() {
+    if (questionIndex == questions.length) {
+        win();
+        return;
+    }
+    mus.load();
+    mus.play();
+    //change the content of the question and the answers
+    question.innerHTML = questions[questionIndex].question;
+    answer1.innerHTML = questions[questionIndex].answer1;
+    answer2.innerHTML = questions[questionIndex].answer2;
+    answer3.innerHTML = questions[questionIndex].answer3;
+
+    ///
+    var anim = "bounceInUp";
+    //debugger;
+    testAnim(anim);
+
+    //reset the old value answer
+    answer1.value = 0;
+    answer2.value = 0;
+    answer3.value = 0;
+
+    //update the current correct answer
+    //document.getElementById("ans" + questions[questionIndex].correctAnswer).value = 1;
+
+    questionIndex++;
+    moveProgress(userTimeUntilMusicStop);
+    setTimeout(
+        function () {
+            mus.pause();
+            stopper(userTimeUntilViewAnswer, viewCorrectAnswer);
+        }, userTimeUntilMusicStop * 1000 + 1000);
+
+
+}
+function viewCorrectAnswer() {
+    document.getElementById('id01').style.display = 'block';
+    //alert("viewCorrectAnswer" + questions[questionIndex - 1].correctAnswer);
+    var correct = document.getElementById("correctAnswer");
+    switch (questions[questionIndex - 1].correctAnswer) {
+        case 1: correct.innerHTML = questions[questionIndex - 1].answer1;
+            break;
+        case 2: correct.innerHTML = questions[questionIndex - 1].answer2;
+            break;
+        case 3: correct.innerHTML = questions[questionIndex - 1].answer3;
+            break;
+        default: correct.innerHTML = "תשובה 4";
+    }
+}
+function win() {
+    mus.load();
+    mus.play();
+    if (pointsGroup1 > pointsGroup2) {
+        document.getElementById("main").innerHTML = ' <h1 class="win">הקבוצה המנצחת היא קבוצה א</h1><canvas width="1920" height="448"></canvas>'
+    } else if (pointsGroup1 < pointsGroup2) {
+        document.getElementById("main").innerHTML = ' <h1 class="win">הקבוצה המנצחת היא קבוצה ב</h1><canvas width="1920" height="448" ></canvas>'
+    }
+    else {
+        document.getElementById("main").innerHTML = ' <h1 class="win">מספר הנקודות זהה לשתי הקבוצות</h1><canvas width="1920" height="448"></canvas>'
+    }
+    gameOver();
+}
+
+//my stopper
+var sec = userTimeUntilMusicStop;
+var ms = 0;
+var secElem = document.getElementById("sec");
+var msElem = document.getElementById("ms");
+function stopper(time, func) {
+    sec = time;
+    secElem.innerHTML = sec < 10 ? ("0" + sec) : sec;
+    var interval = setInterval(function () {
+
+        if (sec == 0 && ms == 0) {
+            clearInterval(interval);
+            if (func)
+                func();
+            return;
+        }
+
+        if (ms == 0) {
+            msElem.innerHTML = ms = 100;
+            if (sec > 0) {
+                secElem.innerHTML = (--sec < 10) ? ("0" + sec) : sec;
+            }
+        }
+        msElem.innerHTML = (--ms < 10) ? ("0" + ms) : ms;
+    }, 10);
+}
+
+var pb = 0;
+function moveProgress(timeInSec, eachTime = 0.01) {
+    if (pb == 0) {
+        pb = 1;
+        var elem = document.getElementById('myBar');
+        var width = 1;
+        var id = setInterval(frame, eachTime*1000);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                pb = 0;
+            } else {
+                width += eachTime*100/timeInSec;
+                elem.style.width = width + '%';
+            }
+        }
+    }
+}
+
+function cssAnim(elem, anim) {
+    elem.classList.add(anim);
+}
+moveProgress(5, callback);
---
+++ ClassicTrivia.tha/js/data.js
@@ -0,0 +1,87 @@
+﻿var timeUntilMusicStop = 3;
+var timeUntilViewAnswer = 0;
+
+//question: "כאן כתבי את השאלה",
+//answer1: " תשובה1",
+//answer2: " תשובה2",
+//answer3: " תשובה3",
+//correctAnswer: כאן כתבי את מספר התשובה הנכונה
+
+
+var userQuestions = [
+    {
+        question: 'באיזו שנה הוקמה עמותת "גניזה כללית"?',
+        answer1:'תש"ע',
+        answer2: 'תשמ"ו',
+        answer3: 'תשכ"ו',
+        correctAnswer: 2
+    },
+    {
+        question: "מהו הצעצוע הנמכר ביותר בעולם?",
+        answer1: "5 אבנים",
+        answer2: "שחמט",
+        answer3: "קוביה הונגרית",
+        correctAnswer: 3
+    }
+    , {
+        question: "החטיף הנמכר ביותר בישראל?",
+        answer1:"ציפס",
+        answer2:"במבה",
+        answer3:"דוריטוס",
+        correctAnswer: 2
+    }
+    , {
+        question: "מהי הרשת הצעצועים הגדולה ביותר בעולם ?",
+        answer1: "אס",
+        answer2: "פישר פרייס ",
+        answer3: "פוקסמיינד",
+        correctAnswer:1
+    }
+    , {
+        question: "כמה חדרים יש בארמון של הסולטן חסן אל בולקיה ?",
+        answer1: "1788",
+        answer2: "500",
+        answer3: "2153",
+        correctAnswer: 1
+    }
+    , {
+        question: "תוך כמה זמן מתכלה כוס קלקר בים?",
+        answer1: "5 ימים",
+        answer2: "שנה",
+        answer3: "50 שנה",
+        correctAnswer: 3
+    }
+    , {
+        question: "כמה מטרים המזרקה הגדולה ביותר ?",
+        answer1: "400 מטר",
+        answer2: "170 מטר",
+        answer3: "50 מטר",
+        correctAnswer: 2
+    }
+    , {
+        question: " מהו המקור שלגילוף בפרות וירקות?",
+        answer1: "אמריקה",
+        answer2: "ישראל",
+        answer3: "סין",
+        correctAnswer: 3
+    }, {
+        question: "כמה כוסות מים מבוגר צריך לשתות ביום ?",
+        answer1: "8",
+        answer2: "20",
+        answer3: "15",
+        correctAnswer: 1
+    }, {
+        question: "מהו האוכל הנמכר ביותר בישראל ?",
+        answer1: "פלאפל",
+        answer2: "פיצה",
+        answer3: "בגאטוסט",
+        correctAnswer: 2
+    }
+    , {
+        question: "כמה קלוריות יש בטילון גדול ?",
+        answer1: "248",
+        answer2: "444",
+        answer3: "730",
+        correctAnswer: 2
+    }
+]
